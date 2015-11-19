144b0534b84456ede8c6d09595f5713fcd364fb2
Renan Cadaval <renancadaval@gmail.com>
2015-11-19 21:47:16 -0200

    Trying to avoid oom on low memory devices

--- android/src/main/java/org/robovm/store/util/Images.java
+++ android/src/main/java/org/robovm/store/util/Images.java
@@ -77,11 +77,41 @@ public class Images {
     }
 
     private static Bitmap saveBitmap(String url, File imagePath) {
-        Bitmap bmp = BitmapFactory.decodeFile(imagePath.getAbsolutePath());
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(imagePath.getAbsolutePath(), options);
+
+        options.inSampleSize = calculateInSampleSize(options, (int) SCREEN_WIDTH, (int) SCREEN_WIDTH);
+        options.inJustDecodeBounds = false;
+        Bitmap bmp = BitmapFactory.decodeFile(imagePath.getAbsolutePath(), options);
+
         if (bmp != null) {
             bmpCache.put(url, bmp);
         }
         return bmp;
+    }
+
+    public static int calculateInSampleSize(
+            BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height / 2;
+            final int halfWidth = width / 2;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) > reqHeight
+                    && (halfWidth / inSampleSize) > reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
     }
 
     public static void setScreenWidth(float screenWidth) {
